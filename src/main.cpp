54d05b09cb61720d0a2250ced7284f9a00bfad60
Rbussom <ryanbussom@gmail.com>
2025-02-04 15:56:09 -0600

    VLADD can drive

--- src/main.cpp
+++ src/main.cpp
@@ -24,16 +24,14 @@ competition Competition;
 controller Controller = controller();
 
 // Motors for Left Side
-motor Left_Motor1 = motor(PORT4, true);
-motor Left_Motor2 = motor(PORT14, true);
-motor Left_Motor3 = motor(PORT1, true);
-motor_group MotorGroupLeft = motor_group(Left_Motor1, Left_Motor2, Left_Motor3);
+motor Left_Motor1 = motor(PORT11, true);
+motor Left_Motor2 = motor(PORT20, true);
+motor_group MotorGroupLeft = motor_group(Left_Motor1, Left_Motor2);
 
 // Motors for Right Side
-motor Right_Motor1 = motor(PORT7);
+motor Right_Motor1 = motor(PORT1);
 motor Right_Motor2 = motor(PORT10);
-motor Right_Motor3 = motor(PORT18);
-motor_group MotorGroupRight = motor_group(Right_Motor1, Right_Motor2, Right_Motor3);
+motor_group MotorGroupRight = motor_group(Right_Motor1, Right_Motor2);
 
 // Intake Motors
 motor HookIntake = motor(PORT19, true);
@@ -49,148 +47,6 @@ brain Brain;
 led ClampMotor = led(Brain.ThreeWirePort.A);
 led RatchetMotor = led(Brain.ThreeWirePort.B);
 
-// Control Variables
-bool toggle = false;
-bool wasPressing = false;
-bool L1wasPressing = false;
-bool XwasPressing = false;
-bool wasYPressing = false; 
-bool redirectMode = false;
-
-
-//Limit swtich 
-//limit limitSwitch = limit(Brain.ThreeWirePort.H);
-
-//Optiic sensor
-optical Optical17 = optical(PORT17);
-double hue = Optical17.hue();
-//optical mode
-bool isRed = true;
-
-//threading
-vex::thread redirectThread;
-
-
-void intake_Functionality() {
-  while(true){
-
-    // Toggle Intake Motors on Button Up Press
-    if (Controller.ButtonL1.pressing() && !L1wasPressing) {
-      toggle = !toggle;
-    }
-
-    // Hold-to-Reverse functionality for Intake Motors (Button Down)
-    if (Controller.ButtonL2.pressing()) {
-      HookIntake.spin(reverse, 100, percent);
-      FrontIntake.spin(reverse, 100, percent);
-    } else if (toggle) {
-      // Run Intake Motors Forward if toggled
-      HookIntake.spin(fwd, 100, percent);
-      FrontIntake.spin(fwd, 100, percent);
-    } else {
-      // Stop Intake Motors if not toggled and Button Down not pressed
-      HookIntake.stop();
-      FrontIntake.stop();
-    }
-
-    // Check for Y button press to toggle redirect mode
-
-    if (Controller.ButtonY.pressing() && !wasYPressing) {
-      redirectMode = !redirectMode; // Toggle redirect mode on button press
-    }
-    wasYPressing = Controller.ButtonY.pressing(); // Update previous state of Y button
-    L1wasPressing = Controller.ButtonL1.pressing();
-
-
-    // If in redirect mode and the obtical senses color 
-    if (Optical17.isNearObject()) {
-      Optical17.setLightPower(100);
-      Optical17.setLight(ledState::on);
-      
-      if (Optical17.hue() > 180 && Optical17.hue() < 240) {
-        float targetPosition = HookIntake.position(degrees) + 58; // Fine-tune this value
-        float backTargetPosition = targetPosition - 3;
-
-        // Wait until the intake reaches the desired position, then reverse to eject
-        if (Optical17.hue() > 180 && Optical17.hue() < 240 && HookIntake.position(degrees) < targetPosition){
-          waitUntil(HookIntake.position(degrees) > targetPosition);
-        }
-        waitUntil(HookIntake.position(degrees) > targetPosition);
-        HookIntake.spin(reverse, 100, percent);
-        FrontIntake.spin(reverse, 100, percent);
-
-        // Wait until the intake returns to the starting position
-        waitUntil(HookIntake.position(degrees) < backTargetPosition);
-      }
-
-
-
-
-      
-      if(((isRed && Optical17.hue() > 0 && Optical17.hue() < 30) \
-      || (!isRed && Optical17.hue() > 180 && Optical17.hue() < 240)) && redirectMode){
-        float targetPosition = HookIntake.position(degrees) + 20.215; //tuneable
-        float backTargetPosition = HookIntake.position(degrees) - 350;
-        waitUntil(HookIntake.position(degrees) > targetPosition);
-        HookIntake.spin(reverse, 100, percent); // Spin the hook intake
-        FrontIntake.spin(reverse, 100, percent); // Spin the front intake
-        waitUntil(HookIntake.position(degrees) < backTargetPosition);
-        redirectMode = false; // Optionally reset redirect mode after action
-        }
-    }
-    else{
-      Optical17.setLight(ledState::off);
-    }
-    
-    vex::this_thread::sleep_for(10);  // Small delay 
-  }
-}
-
-
-//redirect function (for threading)
-/*
-void redirectMotor() {
-
-  // Perform redirection logic here
-    float targetPosition = HookIntake.position(degrees) + 58; // Adjust as needed
-    float backTargetPosition = HookIntake.position(degrees) - 5;
-
-    // Redirect forward until the target position
-    while (HookIntake.position(degrees) < targetPosition) {
-      HookIntake.spin(fwd, 100, percent);
-      FrontIntake.spin(fwd, 100, percent);
-      this_thread::sleep_for(10);  // Small delay to prevent CPU overload
-    }
-
-    // Reverse the motors for the specified distance
-    while (HookIntake.position(degrees) > backTargetPosition) {
-      HookIntake.spin(reverse, 100, percent);
-      FrontIntake.spin(reverse, 100, percent);
-      this_thread::sleep_for(10);
-    
-
-    // Stop the motors when the redirection is done
-    HookIntake.stop();
-    FrontIntake.stop();
-  }
-}
-*/ 
-
-
-//eject function (used in threading)
-
-/*
-void Eject(){
-        float targetPosition = HookIntake.position(degrees) + 27; //tuneable
-        float backTargetPosition = HookIntake.position(degrees) - 300;
-        waitUntil(HookIntake.position(degrees) > targetPosition);
-        HookIntake.spin(reverse, 100, percent); // Spin the hook intake
-        FrontIntake.spin(reverse, 100, percent); // Spin the front intake
-        //task::sleep(2000); // Wait for 2 seconds
-        waitUntil(HookIntake.position(degrees) < backTargetPosition);
-        redirectMode = false; // Optionally reset redirect mode after action
-}
-*/
 //------------------------------------------------------------------------------
 // Pre-Autonomous Functions
 //------------------------------------------------------------------------------
@@ -205,7 +61,7 @@ void pre_auton(void) {
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
 
-  Optical17.setLightPower(100, percent);
+  
 }
 
 //------------------------------------------------------------------------------
@@ -247,36 +103,8 @@ float readCurrentPosition() {
  * This function should contain the teleoperated control code for the robot.
  */
 void usercontrol(void) {
-  //thread
-  vex::thread t_intake = vex::thread(intake_Functionality);
-  
+
   while (true) {
-    
-    // Toggle Clamp LED on ButtonL1 Press
-    if (Controller.ButtonUp.pressing() && !wasPressing) {
-      ClampMotor.set(!ClampMotor);
-    }
-
-    // Toggle RatchetMotor LED on ButtonX Press
-    if (Controller.ButtonX.pressing() && !XwasPressing) {
-      RatchetMotor.set(!RatchetMotor);
-    }
-
-    
-
-
-    // Update Toggle States
-    wasPressing = Controller.ButtonUp.pressing();
-    XwasPressing = Controller.ButtonX.pressing();
-
-    // Control Claw Motors
-    if (Controller.ButtonR2.pressing()) {
-      ClawMotorGroup.spin(fwd, 100, percent);
-    } else if (Controller.ButtonR1.pressing()) {
-      ClawMotorGroup.spin(reverse, 100, percent);
-    } else {
-      ClawMotorGroup.stop();
-    }
 
     // Drive Control (Tank Drive)
     MotorGroupLeft.spin(fwd, Controller.Axis3.position(), percent);
